8d312da6c757ab648d6c6e57e6789da2700ae291
Mehul Raheja <rahejamehul@gmail.com>
2018-07-19 00:17:58 -0400

    Adafruit and IFTTT
    Added Adafruit + IFTTT to send messages to send messages your phone

--- product2.ino
+++ product2.ino
@@ -1,3 +1,22 @@
+#include <WiFi101.h> 
+#include "Adafruit_MQTT.h" 
+#include "Adafruit_MQTT_Client.h" 
+#define WLAN_SSID       "AirPennNet-Device" 
+#define WLAN_PASS       "penn1740wifi" 
+#define AIO_SERVER      "io.adafruit.com" 
+#define AIO_SERVERPORT  1883 
+#define AIO_USERNAME    "mraheja" 
+#define AIO_KEY         "12ec2606c88d47ec8eb72750f74ed9a7" 
+
+int x = 0;
+WiFiClient client; 
+
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY); 
+Adafruit_MQTT_Publish temperature = Adafruit_MQTT_Publish(&mqtt,  AIO_USERNAME "/feeds/welcome-feed"); 
+
+
+
+
 //PORTS
 const int LED = 1;
 const int BUZZER = 5;
@@ -10,7 +29,7 @@ const int BUTTON = 2;
 int initX = 0, initY = 0, initZ = 0;
 
 //TOLERANCE LEVEL FOR LOW AMOUNT OF MOVEMENT
-int frame = 100;
+int frame = 200;
 
 /*
  * state = 0 IDLE
@@ -35,14 +54,23 @@ void setup() {
   pinMode(BUZZER,OUTPUT);
   pinMode(BUTTON,INPUT);
 
+  connectToWiFi(); 
+  connectToAdafruit(); 
+  
   //Blink the LED to show that the program is running
   digitalWrite(LED,HIGH);
   delay(100);
   digitalWrite(LED,LOW); 
+  
 }
 
 void loop() {
   //Blinks the LED every 0.1 seconds if in initial response or emergency response state
+  if(! mqtt.ping(3)) { 
+   // reconnect to adafruit io 
+   if(! mqtt.connected()) 
+     connect(); 
+ } 
   if(blinking){
     if(millis() > prev + t){
       prev = millis();
@@ -74,8 +102,19 @@ void loop() {
       //If it is, then initialize buzzer and blinking
       initialResponse();
       //Prints to serial monitor so that python can detect it
+      char b[4]; 
       Serial.println("bad");
-      //Moves to state 2
+      String str = "1";
+      for(int i=0; i<str.length(); i++) 
+      { 
+        b[i]=str.charAt(i); 
+      } 
+      b[(str.length())+1]=0; 
+      // Publish data 
+      if (!temperature.publish((char*)b)){
+        Serial.println("Sent");
+      }
+          //Moves to state 2
       state++;
     }
   }else if(state == 2){
@@ -102,6 +141,45 @@ void initialResponse(){
   //Starts initial response with buzzer and blinking
   analogWrite(BUZZER,50);
   blinking = 1;
-
 }
+
+void connectToWiFi() 
+{ 
+ // Connect to WiFi access point. 
+ delay(10); 
+ Serial.print(F("Connecting to ")); 
+ Serial.println(WLAN_SSID); 
+ WiFi.begin(WLAN_SSID, WLAN_PASS); 
+ while (WiFi.status() != WL_CONNECTED) { 
+   delay(500); 
+   Serial.print(F(".")); 
+ } 
+ Serial.println(F("WiFi connected!")); 
+} 
+
+void connectToAdafruit() 
+{ 
+ connect(); 
+} 
+
+void connect() { 
+ Serial.print(F("Connecting to Adafruit IO... ")); 
+ int8_t ret; 
+ while ((ret = mqtt.connect()) != 0) { 
+   switch (ret) { 
+     case 1: Serial.println(F("Wrong protocol")); break; 
+     case 2: Serial.println(F("ID rejected")); break; 
+     case 3: Serial.println(F("Server unavail")); break; 
+     case 4: Serial.println(F("Bad user/pass")); break; 
+     case 5: Serial.println(F("Not authed")); break; 
+     case 6: Serial.println(F("Failed to subscribe")); break; 
+     default: Serial.println(F("Connection failed")); break; 
+   } 
+   if(ret >= 0) 
+     mqtt.disconnect(); 
+   Serial.println(F("Retrying connection...")); 
+   delay(5000); 
+ } 
+ Serial.println(F("Adafruit IO Connected!")); 
+} 
 
